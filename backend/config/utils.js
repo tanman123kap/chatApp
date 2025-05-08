f75f2d088ae23af1e5b96fa788fa65a00ae19436
tanman123kap <tanman123kap@gmail.com>
2025-05-08 15:10:46 +0530

    Update utils.js

--- backend/config/utils.js
+++ backend/config/utils.js
@@ -9,7 +9,7 @@ const generateToken = async (userId, res) => {
         res.cookie("token", token, {
             maxAge: 2 * 24 * 60 * 60 * 1000, //MS
             httpOnly: true, //prevent XSS attacks cross-site scripting attacks
-            sameSite: "strict", //CSRF attacks cross-site request forgery attacks
+            sameSite: "none",
             secure: process.env.NODE_ENV !== "development"
         });
         return token;
